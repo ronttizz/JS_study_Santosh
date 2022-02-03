a92b7c84da083bf2896b7cb2d7aec3098c6b6fbc
Nissinen Toni <s2200129@BC-C02GR0BWQ6LR-MacBook-Air.local>
2022-02-03 13:29:17 +0200

    Optional chaining tasks

---
+++ Practice_lessons3101_v2/optional/optional03.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/**
+Re-write the getFirstGrade function without using if conditions.
+ * @param {object} data
+ * @param {object} [data.results]
+ * @param {number[]} [data.results.grades]
+ */
+const getFirstGrade = (data) => {
+  return data.results?.grades?.[0];
+};
+
+// Sample usage - do not modify
+console.log(getFirstGrade({ results: { grades: [18, 14, 19] } })); // 18
+console.log(getFirstGrade({ results: {} })); // undefined
+console.log(getFirstGrade({})); // undefined
