1671f8c273733d929e320a814a42baf78fdca49b
Nissinen Toni <s2200129@BC-C02GR0BWQ6LR-MacBook-Air.local>
2022-02-03 10:55:32 +0200

    More classroom testing

--- Classroom/test.js
+++ Classroom/test.js
@@ -7,7 +7,7 @@
 //   console.log("Failing grade");
 // }
 
-let age;
+// let age;
 // function canVote(age) {
 //   if (age >= 25) {
 //     return true;
@@ -22,7 +22,27 @@ let age;
 
 // console.log(canVote(26));
 
-const items = ["Pen", "Paper", "staples"];
-const deletedItems = items.splice(0, 1);
-console.log(deletedItems);
-console.log(items);
+// const items = ["Pen", "Paper", "staples"];
+// const deletedItems = items.splice(0, 1);
+// console.log(deletedItems);
+// console.log(items);
+
+// const grades = [5, 2, 10];
+// const sum = grades.reduce((total, current) => {
+//   return total * current;
+// }, 1);
+
+// console.log(sum);
+
+// const dimensions = [20, 5, 45];
+
+// const [width, height, xyz] = dimensions;
+
+// console.log(width);
+// console.log(height);
+// console.log(xyz);
+
+const lat = [5.234];
+const lng = [1.412];
+const point = [...lat, ...lng];
+console.log(point); // [5.234, 1.412];
