7faf5819a36e0b1087e98f5f956edb478cbe4f44
ronttizz <toni.nissinen@gmail.com>
2022-02-08 10:38:07 +0200

    Practices about nullish coalescing

---
+++ Practice_lessons070222/nullish/nullish02.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+Refactor the getWelcomeMessage function without using an if condition.
+The functionality should stay the same.
+*/
+/**
+ * @param {object} user
+ * @param {string} [user.fullName]
+ */
+const getWelcomeMessage = (user) => {
+  return `Welcome ${user.fullName ?? "user"}`;
+};
+
+// Sample usage - do not modify
+console.log(getWelcomeMessage({ fullName: "Sam Green" })); // "Welcome Sam Green"
+console.log(getWelcomeMessage({})); // "Welcome user"
