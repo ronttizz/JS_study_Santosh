bb3e1a8a0bc73fda1bc28eaf0ba32be8b3e2efd5
ronttizz <toni.nissinen@gmail.com>
2022-02-14 13:57:09 +0200

    Class practice examples

---
+++ Practice_lessons11402/classes/class2.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+Define a class (empty for now) that represents a recipe. Then create a new variable called recipe and
+assign it to a new instance of that class.
+**/
+
+class Recipe {
+  // empty recipe class
+}
+
+let pulla = new Recipe();
+console.log(pulla);
---
+++ Practice_lessons11402/classes/class4.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**
+Write the Recipe class such that it logs the name and the calories every time
+a new instance of the class is being created..
+**/
+
+// write class definition
+class Recipe {
+  constructor(name, calories) {
+    console.log(name, calories);
+  }
+}
+
+// Class usage
+let pasta = new Recipe("Pasta", 600);
+console.log("---");
+let salad = new Recipe("Salad", 400);
