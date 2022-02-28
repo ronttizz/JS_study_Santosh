7522efcaef434f02f9a9c877c6058aa74ef9ef93
ronttizz <toni.nissinen@gmail.com>
2022-02-28 11:57:06 +0200

    Promise practises

---
+++ Practice_lessons1502/promises/promise10/promise10.js
@@ -0,0 +1,22 @@
+// Sample usage (do not modify)
+const waitForCompleteClick = () => {
+  return new Promise((resolve, reject) => {
+    document.querySelector("#complete-btn").addEventListener("click", () => {
+      resolve();
+    });
+    document.querySelector("#fail-btn").addEventListener("click", () => {
+      reject();
+    });
+  });
+};
+
+/* Update the code in index.js such that the console.log("Complete clicked"); runs after you click on the complete button and console.error("Fail clicked") runs after you click on the fail button.
+Unlike the wait() function, this function does not take any parameters.*/
+
+waitForCompleteClick()
+  .then(() => {
+    return console.log("Complete clicked");
+  })
+  .catch(() => {
+    return console.error("Fail Clicked");
+  });
