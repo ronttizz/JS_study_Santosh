527bac45d6ef090046622ae8a5426ae45f180773
ronttizz <toni.nissinen@gmail.com>
2022-03-07 13:43:08 +0200

    async/await practices

---
+++ async_await/async_await03.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/*
+Rewrite the canVote function as an async arrow function.
+*/
+
+/**
+ * @param {number} age
+ */
+const canVote = async (age) => {
+  return new Promise((resolve) => {
+    resolve(age >= 18);
+  });
+};
+
+// Sample usage - do not modify
+canVote(20).then((result) => {
+  console.log(result); // true
+});
+canVote(15).then((result) => {
+  console.log(result); // false
+});
