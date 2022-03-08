ffe3b648aa328ef18041fdc2a4e635b2405489e2
ronttizz <toni.nissinen@gmail.com>
2022-03-08 10:40:41 +0200

    Scope practices

---
+++ Practice_lessons0103/misc/misc2/misc2.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/**
+Every time you click on the Start game button, it is currently calling the startGame() and logAnalytics() functions.
+Update the event listener such that it only calls the startGame() once, but keeps calling logAnalytics() every time.
+**/
+
+const button = document.querySelector("#app-button");
+let clicked = false;
+button.addEventListener("click", () => {
+  if (clicked !== true) {
+    startGame();
+    clicked = true;
+  } else {
+    logAnalytics();
+  }
+});
+
+// do not modify
+function startGame() {
+  console.log("game started!");
+}
+
+// do not modify
+function logAnalytics() {
+  console.log("log analytics");
+}
